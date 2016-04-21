e99856477e17813f176b7923336a0e8355fb7d0c
shaon3343 <shaon.ashfak@gmail.com>
2016-04-21 14:45:52 +0600

    fist commit

---
+++ app/models/Product.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import play.db.ebean.Model;
+
+@Entity
+@Table(name="product")
+public class Product extends Model {
+	@Id
+	public Long id;
+	
+	@Column(name = "product_name")
+	public String productName;
+	
+	@Column(name = "product_code")
+	public String productCode;
+	
+	@Column(name = "product_price")
+	public float productPrice;
+	
+	private static Finder<Integer, Product> find = new Finder<>(Integer.class, Product.class);
+	
+	public static List<Product> all() {
+		return find.all();
+	}
+
+	public static Product findById(int id){
+		return find.byId(id);
+
+	}
+}
