df21bf049e0cffc7f78b975c80942194500c3529
shaon3343 <shaon.ashfak@gmail.com>
2016-08-08 01:09:14 +0600

    after doing Jquery search for tr td textarea and setting value

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,37 +4,46 @@
 # --- !Ups
 
 create table product (
-  id                        number(19) not null,
-  product_name              varchar2(255),
-  product_code              varchar2(255),
-  product_price             number(19,4),
-  product_quantity          number(19,4),
+  id                        bigint auto_increment not null,
+  product_name              varchar(255),
+  product_code              varchar(255),
+  product_price             float,
+  product_quantity          float,
   constraint pk_product primary key (id))
 ;
 
-create table sales_man (
-  id                        number(19) not null,
-  sales_name                varchar2(255),
-  sales_address             varchar2(255),
-  sales_contact             varchar2(255),
-  sales_tot_due             number(19,4),
-  constraint pk_sales_man primary key (id))
+create table receipts (
+  id                        bigint auto_increment not null,
+  product_id                bigint,
+  product_quantity          float,
+  sales_man_id              bigint,
+  sales_date                datetime,
+  total_due                 float,
+  total_price               float,
+  constraint pk_receipts primary key (id))
 ;
 
-create sequence product_seq;
-
-create sequence sales_man_seq;
+create table sales_man (
+  id                        bigint auto_increment not null,
+  sales_name                varchar(255),
+  sales_address             varchar(255),
+  sales_contact             varchar(255),
+  sales_tot_due             float,
+  constraint pk_sales_man primary key (id))
+;
 
 
 
 
 # --- !Downs
 
-drop table product cascade constraints purge;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table sales_man cascade constraints purge;
+drop table product;
 
-drop sequence product_seq;
+drop table receipts;
 
-drop sequence sales_man_seq;
+drop table sales_man;
+
+SET FOREIGN_KEY_CHECKS=1;
 
